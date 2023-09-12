b6b1f2889f1262e5ef9f8c09bfdc1b1ef5ff55c7
serifcolakel <serifcolakel0@gmail.com>
2023-09-12 19:19:21 +0300

    INFO (serif) : tailwind added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../../vite.svg';
-import './App.css';
+import viteLogo from '../public/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
+    <div className="w-full h-screen flex flex-col items-center justify-center gap-y-4 bg-gradient-to-tr from-blue-400 to-purple-500 text-white p-4">
+      <div className="flex flex-col items-center justify-center gap-y-4">
         <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
           <img alt="Vite logo" className="logo" src={viteLogo} />
         </a>
@@ -16,9 +15,12 @@ function App() {
           <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className="text-3xl font-bold underline bg-gray-600 p-4 rounded-lg">
+        Vite + React
+      </h1>
+      <div className="flex flex-col items-center justify-center gap-y-4">
         <button
+          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
           onClick={() => setCount((prevCount) => prevCount + 1)}
           type="button"
         >
@@ -31,7 +33,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
